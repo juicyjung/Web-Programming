a0e7a87c3064aeb4b647473177f50d378d4b73a3
juicy_jung <83687471+juicyjung@users.noreply.github.com>
2021-06-01 00:55:10 +0900

    Add files via upload

---
+++ 39page.php
@@ -0,0 +1,12 @@
+<?php
+$foo = "foobar";
+$bar = "barbaz";
+
+echo $foo; // foobar
+echo $foo,$bar;
+echo "<br>";
+echo 'This ' , 'string ' , 'was ' , 'made ' , 'with multiple parameters.' , 
+"<br>";
+echo 'This ' . 'string ' . 'was ' . 'made ' . 'with concatenation.' . "<br>";
+echo "<br>";
+?>
