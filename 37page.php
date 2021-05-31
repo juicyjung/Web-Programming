fa804166d34e3eb5ce3b6665197245fcb64fc9b5
juicy_jung <83687471+juicyjung@users.noreply.github.com>
2021-06-01 00:55:01 +0900

    Add files via upload

---
+++ 37page.php
@@ -0,0 +1,11 @@
+<?php
+$foo = "foobar";
+$bar = "barabz";
+echo "foo is $foo";
+echo "<br>";
+$baz = array("value" => "foo");
+echo "this is {$baz['value']}!";
+echo "<br>";
+echo 'foo is $foo';
+echo "<br>";
+?>
