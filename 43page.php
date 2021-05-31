704cadf21d9b517195cbb3871581cfa9c14b28a1
juicy_jung <83687471+juicyjung@users.noreply.github.com>
2021-06-01 00:55:18 +0900

    Add files via upload

---
+++ 43page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$x = 100; $y = "100";
+var_dump($x == $y);
+var_dump($x === $y);
+echo "<br/>";
+var_dump($x != $y);
+var_dump($x !== $y);
+?>
+</body>
+</html>
